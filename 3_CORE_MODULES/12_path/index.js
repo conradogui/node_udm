fa06edb20261218ef618c8e5cbeb895306908124
conradogui <gui14fbce@gmail.com>
2024-10-28 16:15:11 -0300

    apredendo fundamentos node

---
+++ 3_CORE_MODULES/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require("path")
+
+const customPath = '/relatorios/guilherme/relatorios.pdf'
+
+console.log(path.dirname(customPath))
+console.log(path.basename(customPath))
+console.log(path.extname(customPath))
