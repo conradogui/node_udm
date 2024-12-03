d9aee2898d0a29594036198ca10ca1123850ddde
conradogui <gui14fbce@gmail.com>
2024-12-03 19:35:18 -0300

    trabalhando no modelo mvc

---
+++ 10_MVC/arqv_unic/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+const conn = require("./db/conn.js");
+
+const Task = require("./models/Task.js");
+
+const tasksRoutes = require('./routes/taskRoutes.js')
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use('/tasks', tasksRoutes )
+
+conn
+  .sync()
+  .then(() => app.listen(3000))
+  .catch((err) => console.log(err));
